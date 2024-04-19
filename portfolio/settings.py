12e77b0938603ea1d320b5ae8a937785c6e0a61d
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-19 22:16:09 +0545

    Refactor(Setting):"Setting Update"

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 from pathlib import Path
 
@@ -124,13 +123,9 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-#activating heroku
-django_heroku.settings(locals())
